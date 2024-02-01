6a03b2826da19d64d675fab327380c6f1300e4d4
codcreater1 <141832088+codcreater1@users.noreply.github.com>
2024-02-01 12:06:56 +0100

    time counter

--- main.py
+++ main.py
@@ -1,47 +1,16 @@
-import random
-import os
+import time
 
 
-def num_die():
-  while True:
-      try:
-          num_dice = input('Number of dice: ')
-          valid_responses = ['1', 'one', 'two', '2']
-          if num_dice not in valid_responses:
-              raise ValueError('1 or 2 only')
-          else:
-              return num_dice
-      except ValueError as err:
-          print(err)
-
-
-def roll_dice():
-   min_val = 1
-   max_val = 6
-   roll_again = 'y'
-
-   while roll_again.lower() == 'yes' or roll_again.lower() == 'y':
-       os.system('cls' if os.name == 'nt' else 'clear')
-       amount = num_die()
-
-       if amount == '2' or amount == 'two':
-           print('Rolling the dice...')
-           dice_1 = random.randint(min_val, max_val)
-           dice_2 = random.randint(min_val, max_val)
-
-           print('The values are:')
-           print('Dice One: ', dice_1)
-           print('Dice Two: ', dice_2)
-           print('Total: ', dice_1 + dice_2)
-
-           roll_again = input('Roll Again? ')
-       else:
-           print('Rolling the die...')
-           dice_1 = random.randint(min_val, max_val)
-           print(f'The value is: {dice_1}')
-
-           roll_again = input('Roll Again? ')
+def countdown(user_time):
+   while user_time >= 0:
+       mins, secs = divmod(user_time, 60)
+       timer = '{:02d}:{:02d}'.format(mins, secs)
+       print(timer, end='\r')
+       time.sleep(1)
+       user_time -= 1
+   print('Lift off!')
 
 
 if __name__ == '__main__':
-   roll_dice()
+   user_time = int(input("Enter a time in seconds: "))
+   countdown(user_time)
