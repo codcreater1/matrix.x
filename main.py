e0a459b4018468314ea3257922192c8d3a99be7f
codcreater1 <141832088+codcreater1@users.noreply.github.com>
2024-02-01 12:04:57 +0100

    dice game

--- main.py
+++ main.py
@@ -1,67 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 import random
-
-name = input("What is your name? ")
-
+import os
 
 
-print("Good Luck ! ", name)
-
-words = ['rainbow', 'computer', 'science', 'programming',
-       'python', 'mathematics', 'player', 'condition',
-       'reverse', 'water', 'board', 'geeks']
-
-
-word = random.choice(words)
-
-print("Guess the characters")
-
-guesses = ''
-turns = 12
-
-while turns > 0:
+def num_die():
+  while True:
+      try:
+          num_dice = input('Number of dice: ')
+          valid_responses = ['1', 'one', 'two', '2']
+          if num_dice not in valid_responses:
+              raise ValueError('1 or 2 only')
+          else:
+              return num_dice
+      except ValueError as err:
+          print(err)
 
 
-    failed = 0
+def roll_dice():
+   min_val = 1
+   max_val = 6
+   roll_again = 'y'
 
+   while roll_again.lower() == 'yes' or roll_again.lower() == 'y':
+       os.system('cls' if os.name == 'nt' else 'clear')
+       amount = num_die()
 
-    for char in word:
+       if amount == '2' or amount == 'two':
+           print('Rolling the dice...')
+           dice_1 = random.randint(min_val, max_val)
+           dice_2 = random.randint(min_val, max_val)
 
+           print('The values are:')
+           print('Dice One: ', dice_1)
+           print('Dice Two: ', dice_2)
+           print('Total: ', dice_1 + dice_2)
 
-       if char in guesses:
-          print(char, end=" ")
-
+           roll_again = input('Roll Again? ')
        else:
-          print("_")
-          failed += 1
+           print('Rolling the die...')
+           dice_1 = random.randint(min_val, max_val)
+           print(f'The value is: {dice_1}')
 
-    if failed == 0:
-
-       print("You Win")
-       print("The word is: ", word)
-       break
+           roll_again = input('Roll Again? ')
 
 
-    print()
-    guess = input("guess a character:")
-
-
-    guesses += guess
-
-
-    if guess not in word:
-
-       turns -= 1
-
-       print("Wrong")
-
-
-       print("You have", + turns, 'more guesses')
-
-       if turns == 0:
-          print("You Loose")
+if __name__ == '__main__':
+   roll_dice()
